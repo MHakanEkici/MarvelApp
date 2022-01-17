d0269c8038bdb30ec95fd98ed13074d53ae8660c
MHakanEkici <nothingman408@hotmail.com>
2022-01-17 20:42:06 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FavouriteProvider from './context/FavouriteContext/FavouriteProvider';
+import ThemeProvider from './context/ThemeContext/ThemeProvider';
+import Navigation from './navigation';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <FavouriteProvider>
+        <Navigation />
+      </FavouriteProvider>
+    </ThemeProvider>
+  );
+}
